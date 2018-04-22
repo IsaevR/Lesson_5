6fc5f9914e547e94f54d9aabcac7da62a899c12c
IsaevR <diplomatic.area@gmail.com>
2018-04-22 10:33:16 +0300

    firstcommit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace shaffle2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] str = { "Alex", "Donald", "Brain", "Tom" };  // инициализация массива строк
+
+            Shaffle data = new Shaffle(str);   // создать (ссылку на) объект типа Shaffe, который принимает  
+                                               // в качестве аргумента массив типа string
+
+            data.DisplayData();                // вывести на экран исходный массив 
+            data.MixData();                    // переставить элементы массива
+            data.DisplayData();                // вывести на экран массив   
+
+        }
+    }
+}
---
+++ Shaffle.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace shaffle2
+{
+    class Shaffle
+    {
+        // Эти члены класса являются закрытыми.
+        private string[] Data;    // массив, содержащий строки  
+
+        // Конструктор построить пустой класс Shaffle для реализации престановок  
+        public Shaffle(string[] arr)
+        {
+            Data = arr; // установить размер для массива
+        }
+
+        // Этот метод выводит на экран массив строк 
+        public void DisplayData()
+        {
+            //for (int i = 0; i < Data.Length; i++) 
+            //    Console.Write(" {0} ", Data[i]);
+
+            // Метод String.Join (String, String[])Сцепляет все элементы
+            // массива строк, помещая между ними заданный разделитель.
+            Console.WriteLine(string.Join(", ", Data));
+        }
+
+        /// метод для перестановки элементов массива
+        public void MixData()
+        {
+            Random rand = new Random();         // "rand" переменная класса "Random" 
+            string temp;                        // для промежуточного хранения значений массива строк
+            int r = 0;                          // переменна для сохраненеия целочисленного рандомного значения
+
+            for (int i = Data.Length - 1; i >= 0; --i)
+            {
+                r = rand.Next(i + 1);           //Next(Int32) Возвращает неотрицательное случайное целое число, 
+                temp = Data[r];                 //которое меньше максимально допустимого значения.
+                Data[r] = Data[i];
+                Data[i] = temp;
+            }
+        }
+
+    }
+}
